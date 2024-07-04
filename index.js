ca267415c3f8cd12a7b0e9e07d6cf56dc46cf039
leticiamendesz <leticiamendesportelateixeirami@gmail.com>
2024-07-04 17:28:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let prompt = require('prompt-sync')();
+//let n1 = Number(prompt('digite sua n1: '));
+//let n2 = Number(prompt('digite sua n2: '));
+//let media = ((n1 + n2)/2);
+
+//if (media >= 6) {
+//console.log (`aprovado com nota ${media}, parabens!!`)}
+
+//else{
+//console.log(`reprovado com nota ${media}`)
+
+//}
+/////////
+
+/*const cadeira = prompt('digite 1 para cadeira vip e 2 para comum: ');
+let valorcadeira;
+let pipoca = prompt('digite 1 para combo de pipoca e 2 para sem pipoca: ')
+let valorpipoca;
+if (cadeira == 1) 
+    valorcadeira = 80
+else 
+    valorcadeira = 40
+if (pipoca == 1)
+    valorpipoca = 30
+else
+    valorpipoca = 0
+console.log(`valor ficou ${valorpipoca + valorcadeira}`)
+*/
+
