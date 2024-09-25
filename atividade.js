51e577e1c2aa79c0f3525fc89785776822151615
leticiamendesz <leticiamendesportelateixeirami@gmail.com>
2024-09-25 10:02:04 -0300

    atividade.js

--- atividade.js
+++ atividade.js
@@ -12,8 +12,15 @@ let numb2 = parseInt(prompt('digite um segundo numero: '));
 const soma = numb1 + numb2;
 const sub = numb1 - numb2;
 const mult = numb1 * numb2.toFixed(2);
-const div = numb1 / numb2.toFixed(2);
+const div = numb1  / numb2.toFixed(2);
 console.log('A soma é ${soma}');
 console.log('A subtração é ${sub}');
 console.log('A multiplicação é ${mult}');
 console.log('A divisão é ${div}');
+
+let base = parseInt(prompt('digite a base do triangulo: '));
+let altura = parseInt(prompt('Digite a altura do triangulo: '));
+const area = ((base * altura)/2).toFixed(2);
+console.log('A base do triangulo é: ${base}');
+console.log('A altura altura do triangulo é: ${altura}');
+console.log('A área do triangulo é: ${area}');
