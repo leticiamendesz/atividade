bdb7adae9a819a086d93fad0d5aabb495f01c0ca
leticiamendesz <leticiamendesportelateixeirami@gmail.com>
2024-09-25 09:20:50 -0300

    atividade.js

--- atividade.js
+++ atividade.js
@@ -1,1 +1,8 @@
+let nome = prompt('Digite seu nome: ');
+let idade = prompt('Digite sua idade: ');
+console.log('Seu nome é ${nome} e voce tem ${idade} anos!');
+console.log('seu nome é: '+nome+' e voce tem '+idade+' anos!');
 
+let cidade = prompt('Digite a cidade onde voce mora: ');
+console.log('${nome} mora em ${cidade}!');
+console.log('${nome} + mora em '+cidade+'!');
